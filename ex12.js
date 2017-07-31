247318b8649b656f65dae1a39a0a91c1755340f9
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-07-31 23:48:41 +0200

    exercise finished

--- ex12.js
+++ ex12.js
@@ -1,1 +1,12 @@
-// 12. Creating HTML II Write a node.JS program that create a HTML file with a JavaScript function inside that say: "Hello!" Access this HTML with http://localhost:8080 from your browser
+// 12. Creating HTML II Write a node.JS program that
+// create a HTML file with a JavaScript function inside that say: "Hello!"
+
+var http = require('http')
+var fs = require('fs')
+
+http.createServer(function (req, res) {
+  console.log('request was made: ' + req.url)
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf-8')
+  myReadStream.pipe(res)
+}).listen(8080)
